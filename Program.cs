3c6ac43089bf04e15abd26df00e8e694588ac50e
ilaydAkszc <iaksozcu@gmail.com>
2025-08-04 12:00:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using Insecure_Bank.Models.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//***********
+// Connection string'i appsettings.json dosyasýndan al
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// DbContext'i servislere ekle
+builder.Services.AddDbContext<BankaDbContext>(options =>
+    options.UseSqlite(connectionString));
+//***********
+
+// Session ekleniyor
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//*************
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Session middleware aktif ediliyor
+app.UseSession();
+//***********
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
